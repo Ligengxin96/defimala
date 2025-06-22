d5bacc32b541456e07af04a308ff8b3f2b78091f
Ligengxin <Ligengxin96@gmail.com>
2025-06-22 11:49:14 +0800

    Fix formatting of numeric literals in TTToken and TTTokenTest contracts

--- src/TT.sol
+++ src/TT.sol
@@ -5,12 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TTToken is ERC20, Ownable {
-    uint256 private constant INITIAL_SUPPLY = 1_000_000 * 10**6;
-    
+    uint256 private constant INITIAL_SUPPLY = 1_000_000 * 10 ** 6;
+
     constructor() ERC20("Test Token", "TT") Ownable(msg.sender) {
         _mint(msg.sender, INITIAL_SUPPLY);
     }
-    
+
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
--- test/TT.t.sol
+++ test/TT.t.sol
@@ -12,33 +12,33 @@ contract TTTokenTest is Test {
     }
 
     function test_InitialSupply() public {
-        assertEq(ttToken.totalSupply(), 1_000_000 * 10**6);
+        assertEq(ttToken.totalSupply(), 1_000_000 * 10 ** 6);
     }
 
     function test_Mint() public {
         address user1 = makeAddr("user1");
-        ttToken.mint(user1, 100 * 10**6);
-        assertEq(ttToken.balanceOf(user1), 100 * 10**6);
+        ttToken.mint(user1, 100 * 10 ** 6);
+        assertEq(ttToken.balanceOf(user1), 100 * 10 ** 6);
     }
 
     function test_Burn() public {
         address user2 = makeAddr("user2");
-        ttToken.mint(user2, 100 * 10**6);
+        ttToken.mint(user2, 100 * 10 ** 6);
         vm.prank(user2);
-        ttToken.burn(50 * 10**6);
-        assertEq(ttToken.balanceOf(user2), 50 * 10**6);
+        ttToken.burn(50 * 10 ** 6);
+        assertEq(ttToken.balanceOf(user2), 50 * 10 ** 6);
     }
 
     function test_Transfer() public {
         address user3 = makeAddr("user3");
         address user4 = makeAddr("user4");
-        ttToken.mint(user3, 100 * 10**6);
-        
+        ttToken.mint(user3, 100 * 10 ** 6);
+
         vm.prank(user3);
-        bool success = ttToken.transfer(user4, 50 * 10**6);
-        
+        bool success = ttToken.transfer(user4, 50 * 10 ** 6);
+
         assertTrue(success);
-        assertEq(ttToken.balanceOf(user3), 50 * 10**6);
-        assertEq(ttToken.balanceOf(user4), 50 * 10**6);
+        assertEq(ttToken.balanceOf(user3), 50 * 10 ** 6);
+        assertEq(ttToken.balanceOf(user4), 50 * 10 ** 6);
     }
 }
